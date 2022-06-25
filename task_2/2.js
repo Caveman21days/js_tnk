3a3bfb1ba7f3a1c6559b447b4a061b75814ab63e
kirill <kotkidach@naumen.ru>
2022-06-25 13:28:40 +0500

    task_2 little fix 2

--- task_2/2.js
+++ task_2/2.js
@@ -1,6 +1,6 @@
 // 2 - Реализовать функцию для сравнения двух массивов, сравнение должно попарно сравнивать каждый элемент
 
-const _compact = (e) => e
+const _compare = (arr1, arr2) => arr1.every((item, i) => item === arr2[i])
 
 // Сравнение "каждый с каждым"
 const pairComp = function(arr1, arr2) {
@@ -9,11 +9,7 @@ const pairComp = function(arr1, arr2) {
 
 // Сравнение "С индексом"
 const pairComp2 = function(arr1, arr2) {
-  // Как тут можно было написать через итератор?
-  arr1 = arr1.filter(_compact)
-  arr2 = arr2.filter(_compact)
-
-  return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])
+  return _compare(arr1, arr2) && _compare(arr2, arr1)
 }
 
 // В задании имеет смысл привести примеры попарного сравнения. Например, в ruby вычитание зеркальных массивов вернет пустой массив
