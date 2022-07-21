3a0f25bb6d90cfc88a5df78f06e7c7c1ff8a7c08
kirill <kotkidach@naumen.ru>
2022-07-21 19:52:28 +0500

    task_7 done

---
+++ task_7/2/index.js
@@ -0,0 +1,81 @@
+// Реализовать калькулятор
+//
+// В качестве основы для доработки можно взять калькулятор,
+// который мы рассматривали на лекции, либо сделать свой самостоятельно
+//
+// Должна поддерживаться следующая функциональность:
+//   По нажатию на backspace символы в поле ввода должны стираться по одному
+//   По нажатии на enter - срабатывать действие '='
+//
+// Должны поддерживаться базовые арифметические операции: +, - , *, /
+// Добавлять свои нестандартные операции и отображать кнопки для этих операций на экране
+
+import "./styles.css";
+
+const result = document.querySelector(".result");
+
+let previousOperand = null;
+let currentOperand = null;
+let currentOperation = null;
+
+const o = {
+  "+": function (a, b) {
+    return a + b;
+  },
+  "-": function (a, b) {
+    return a - b;
+  },
+  "*": function (a, b) {
+    return a * b;
+  },
+  "/": function (a, b) {
+    return a / b;
+  }
+};
+
+const perform = (clear = true) => {
+  if (o[currentOperation]) {
+    currentOperand = o[currentOperation](+previousOperand, +currentOperand);
+    result.innerHTML = currentOperand;
+    if (clear) previousOperand = null;
+  }
+};
+
+const handleInput = function (val) {
+  switch (val) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      if (previousOperand && currentOperand) perform(false);
+      previousOperand = currentOperand;
+      currentOperand = null;
+      currentOperation = val;
+      result.innerHTML = "";
+      break;
+    case "=":
+    case "Enter":
+      perform();
+      break;
+    case "C":
+      previousOperand = null;
+      currentOperand = null;
+      currentOperation = null;
+      result.innerHTML = "";
+      break;
+    case "Backspace":
+      currentOperand = currentOperand.slice(0, -1);
+      result.textContent = currentOperand;
+      break;
+    default:
+      if (Number.isInteger(+val)) {
+        currentOperand = currentOperand === null ? val : currentOperand + val;
+        result.textContent = currentOperand;
+      }
+  }
+};
+
+document.getElementById("buttonsBlock")
+        .addEventListener("click", (e) => handleInput(e.target.textContent));
+
+document.addEventListener("keydown", (e) => handleInput(e.key));
