bafd2657977525a5cdbbad54f7109412d4dd84fb
kirill <kotkidach@naumen.ru>
2022-07-21 22:54:48 +0500

    change files structure

--- task_7/index.js
+++ task_7/index.js
@@ -1,3 +1,28 @@
+// Реализовать механизм защиты от копирования текстового контента на странице.
+//
+//   Для этого нужно:
+//   Запретить копировать и вырезать текст
+//
+// Запретить использовать контекстное меню
+//
+// При каждой попытке копирования текста или попытке использования
+// на экран должно выводиться сообщение ( можно использовать встроенный alert)
+
+window.addEventListener('cut', function(e) {
+  e.preventDefault()
+  alert('cutting not allowed!')
+})
+
+window.addEventListener('copy', function(e) {
+  e.preventDefault()
+  alert('copying not allowed!')
+})
+
+window.addEventListener('contextmenu', function(e) {
+  e.preventDefault()
+  alert('contextmenu not allowed!')
+})
+
 // Реализовать калькулятор
 //
 // В качестве основы для доработки можно взять калькулятор,
