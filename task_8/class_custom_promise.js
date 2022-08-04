11fe1a0c7848d29862c4effba86804700416a4f2
kirill <kotkidach@naumen.ru>
2022-08-05 00:04:27 +0500

    add second realization by class

--- task_8/class_custom_promise.js
+++ task_8/class_custom_promise.js
@@ -49,7 +49,7 @@ class CustomPromiseByClass {
   }
 
   static resolve = function(arg) {
-    if (arg instanceof CustomPromiseByClass) {
+    if (arg instanceof CustomPromiseByClass || arg instanceof Promise) {
       return new CustomPromiseByClass(resolve => resolve(arg.result))
     }
 
@@ -62,5 +62,130 @@ class CustomPromiseByClass {
 
   static reject = function(arg) {
     new CustomPromiseByClass((_, reject) => reject(arg))
+  }
+}
+
+
+
+
+
+const STATE = {
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected',
+  PENDING: 'pending'
+}
+
+// Кастомный промис, реализованный через классы
+class MyPromise {
+  #thenCbs = []
+  #catchCbs = []
+  #state = STATE.PENDING
+  #value
+  #onResolveBind = this.#onResolve.bind(this)
+  #onRejectBind = this.#onReject.bind(this)
+
+  constructor(executor) {
+    try {
+      executor(this.#onResolveBind, this.#onRejectBind)
+    } catch(e) {
+      this.#onReject(e)
+    }
+  }
+
+  #runCallbacks() {
+    if (this.#state === STATE.FULFILLED) {
+
+      this.#thenCbs.forEach(cb => {
+        cb(this.#value)
+      })
+
+      this.#thenCbs = []
+    }
+
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCbs.forEach(cb => {
+        cb(this.#value)
+      })
+
+      this.#catchCbs = []
+    }
+  }
+
+  #onResolve(val) {
+    setTimeout(() => {
+      if (this.#state !== STATE.PENDING) return
+
+      if (val instanceof MyPromise) {
+        val.then(this.#onResolveBind, this.#onRejectBind)
+        return
+      }
+
+      this.#value = val
+      this.#state = STATE.FULFILLED
+      this.#runCallbacks()
+    }, 0)
+  }
+
+  #onReject(val) {
+    setTimeout(() => {
+      if (this.#state !== STATE.PENDING) return
+
+      if (val instanceof MyPromise) {
+        val.then(this.#onResolveBind, this.#onRejectBind)
+        return
+      }
+
+      this.#value = val
+      this.#state = STATE.REJECTED
+      this.#runCallbacks()
+    }, 0)
+  }
+
+  then(thenCb, catchCb) {
+    return new MyPromise((resolve, reject) => {
+      this.#thenCbs.push(res => {
+        if (thenCb == null) {
+          resolve(res)
+          return
+        }
+
+        try {
+          resolve(thenCb(res))
+        } catch(e) {
+          reject(e)
+        }
+      })
+
+      this.#catchCbs.push(res => {
+        if (catchCb == null) {
+          reject(res)
+          return
+        }
+
+        try {
+          resolve(catchCb(res))
+        } catch(e) {
+          reject(e)
+        }
+      })
+
+      this.#runCallbacks()
+    })
+  }
+
+  catch(callback) {
+    return this.then(null, callback)
+  }
+
+  static resolve(value) {
+    return new Promise(resolve => {
+      resolve(value)
+    })
+  }
+
+  static reject(value) {
+    return new Promise((resolve, reject) => {
+      reject(value)
+    })
   }
 }
